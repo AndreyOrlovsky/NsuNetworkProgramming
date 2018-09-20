a7135bfc821e59e7229d1c1c4d9158370d554b73
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2018-09-20 14:34:49 +0700

    almost done

--- UdpMulticast/UdpMulticast/Program.cs
+++ UdpMulticast/UdpMulticast/Program.cs
@@ -11,40 +11,89 @@ namespace UdpMulticast
 {
     class Program
     {
-        private const int sendPort = 40000;
-        private const int receivePort = 40001;
-        private static readonly Guid guid = Guid.NewGuid();
+        private const int port = 41000;
+        private static readonly Guid myGuid = Guid.NewGuid();
 
-        private static IPAddress multicastGroup = IPAddress.Parse("234.212.200.1"); //default
-        private static IPAddress joinedCopyAddress = null;
-        private static readonly UdpClient udpSender = new UdpClient(sendPort);
-        private static readonly UdpClient udpReceiver = new UdpClient(receivePort);
-        private static readonly IPEndPoint endPoint = new IPEndPoint(multicastGroup, sendPort);
+        private static IPAddress multicastGroup = IPAddress.Parse("235.5.5.11"); //default
+        private static IPAddress localIP = GetLocalIP();
 
-        private static List<(Guid, IPAddress)> copies = new List<(Guid, IPAddress)> { (guid, GetLocalIP()) };
+        private static readonly UdpClient udpSender = null;
+        private static readonly UdpClient udpReceiver = null;
+        private static readonly IPEndPoint groupEndPoint = new IPEndPoint(multicastGroup, port);
+        private static IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
+        private static IPEndPoint remoteEndPoint = null;
+
+        private static Dictionary<Guid, IPAddress> copies = new Dictionary<Guid, IPAddress>() { [myGuid] = localIP };
 
         private static string GetJoinMessage(Guid guid) => $"New copy has joined the group {multicastGroup.ToString()}! It's GUID: {guid.ToString()}.";
-        private static string GetLeaveMessage(Guid guid) =>  $"A copy with GUID {guid.ToString()} has left the group {multicastGroup.ToString()}.";
+        private static string GetLeaveMessage(Guid guid) => $"A copy with GUID {guid.ToString()} has left the group {multicastGroup.ToString()}.";
         private static string CopiesCountMessage => $"There are {copies.Count} copies active now! Here are them:";
+        private static string CopiesListing => string.Join("\n", copies.Select(copy => $"{copy.Key} {copy.Value}"));
+        private const string delimiters = "--------------------------------";
+        private static bool trackedSelf = false;
+
+        static Program()
+        {
+            udpSender = new UdpClient();
+            udpSender.ExclusiveAddressUse = false;
+            udpSender.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSender.Client.Bind(localEndPoint);
+
+            udpReceiver = new UdpClient();
+            udpReceiver.ExclusiveAddressUse = false;
+            udpReceiver.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpReceiver.Client.Bind(localEndPoint);
+        }
 
         public static void Main(string[] args)
         {
+            udpSender.JoinMulticastGroup(multicastGroup, timeToLive: 1);
 
+            Thread thread = new Thread(TrackCondition);
+            thread.Start();
 
-            Console.ReadKey();
-            udpSender.JoinMulticastGroup(multicastGroup);
-
-            new Thread(TrackCondition).Start();
-
-            byte[] message = Encoding.ASCII.GetBytes(guid.ToString());
-            udpSender.Send(message, message.Length, endPoint);
+            SendMessageToGroup("add");
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-            
-            udpSender.Send(message, message.Length, endPoint); //отправлен guid, который уже отправляли - это сигнал о завершении 
+
+            SendMessageToGroup("remove"); 
+
+            Environment.Exit(0);
 
 
+        }
+
+        public static void SendMessageToGroup(string request)
+        {
+            byte[] message = Encoding.ASCII.GetBytes($"{request} {myGuid.ToString()}");
+            udpSender.Send(message, message.Length, groupEndPoint);
+        }
+
+        public static void TrackCondition()
+        {
+            udpReceiver.JoinMulticastGroup(multicastGroup, timeToLive: 1);
+            while (true)
+            {
+                string[] message = Encoding.ASCII.GetString(udpReceiver.Receive(ref remoteEndPoint)).Split(' ');
+                string request = message[0];
+                Guid remoteGuid = Guid.Parse(message[1]);
+
+                if (request == "add" && !(remoteEndPoint.Address.Equals(localIP) && trackedSelf))
+                {
+                    trackedSelf = true;
+                    copies[remoteGuid] = remoteEndPoint.Address;
+                    SendMessageToGroup("add"); //извещаем новую копию о себе
+                    Console.WriteLine(string.Join("\n", GetJoinMessage(remoteGuid), CopiesCountMessage, CopiesListing, delimiters));
+
+                }
+                else if (request == "remove")
+                {
+                    copies.Remove(remoteGuid);
+                    Console.WriteLine(string.Join("\n", GetLeaveMessage(remoteGuid), CopiesCountMessage, CopiesListing, delimiters));
+
+                }
+            }
         }
 
         private static IPAddress GetLocalIP()
@@ -59,12 +108,6 @@ namespace UdpMulticast
             return localIP;
         }
 
-        public static void TrackCondition()
-        {
-            while (true)
-            {
-                //udpSender.Receive();
-            }
-        }
+
     }
 }
