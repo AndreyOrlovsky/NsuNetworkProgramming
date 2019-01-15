4f1811dc0a59f035eb589ae796d2a06e1f384f6f
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-01-16 02:56:22 +0700

    improvements in autotest

--- TcpFileTransfer/AutomatedTesting/Program.cs
+++ TcpFileTransfer/AutomatedTesting/Program.cs
@@ -19,12 +19,12 @@ namespace AutomatedTesting
 
         private static readonly IEnumerable<string> filesToTestTransfering = new[]
         {
+            "astah-uml-7_2_0-1ff236-jre-64bit-setup.exe",
+            "VSCodeUserSetup-x64-1.28.2.exe",
             "SourceTreeSetup-3.0.8.exe",
             "apktool_2.3.4.exe",
             "APK Easy Tool 1.541 Portable.zip",
-            "VSCodeUserSetup-x64-1.28.2.exe",
             "Git.pptx",
-            "astah-uml-7_2_0-1ff236-jre-64bit-setup.exe",
             "Bytecode-Viewer-2.9.11.exe",
         };
 
@@ -32,11 +32,6 @@ namespace AutomatedTesting
         static void Main()
         {
             var random = new Random();
-            var clientInfo = new ProcessStartInfo
-            {
-                FileName = pathToClient,
-                //CreateNoWindow = true
-            };
 
             Process.Start(pathToServer, "19000");
 
@@ -44,8 +39,13 @@ namespace AutomatedTesting
             {
                 Thread.Sleep(TimeSpan.FromSeconds(random.Next(1, 10)));
 
-                clientInfo.Arguments =
-                    $@"""C:\Users\Андрей\OneDrive\{fileName}"" 127.0.0.1 19000";
+                var clientInfo = new ProcessStartInfo
+                {
+                    FileName = pathToClient,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    Arguments = $@"""C:\Users\Андрей\OneDrive\{fileName}"" 127.0.0.1 19000"
+                };
 
                 Process.Start(clientInfo);
             }
