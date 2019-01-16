9f3a0f222c2bf202929ee696f2e61363c97ee60a
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-01-17 00:48:24 +0700

    added GUI client

--- RestChat/RestClient/Properties/AssemblyInfo.cs
+++ RestChat/RestClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RestClient")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("068cce02-a2d8-450e-8ff9-fee1e58b476a")]
+[assembly: Guid("912549d7-292f-4313-b4d2-5a44a35ff1ad")]
 
 // Version information for an assembly consists of the following four values:
 //
